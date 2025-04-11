863630c9bab9cb7e6861b2bdffe83d9d4bf0b19d
Vikrant Singh <singhvikrantkingcse@gmail.com>
2025-04-11 23:58:52 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Contact form handler
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thank you for reaching out! I will get back to you soon.");
+    this.reset();
+  });
+  
